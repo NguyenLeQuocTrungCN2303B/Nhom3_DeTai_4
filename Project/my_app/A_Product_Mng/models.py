0d1f61a1fb13de302a4cc418d245293926ae0714
anh phu <phudz25022005@gmail.com>
2025-02-02 16:34:17 +0700

    create models

--- Project/my_app/A_Product_Mng/models.py
+++ Project/my_app/A_Product_Mng/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product (models.Model):
+    name = models.CharField(max_length=225)
+    kind = models.CharField(max_length=225)
+    weight = models.FloatField()
+class customer (models.Model):
+    name = models.CharField(max_length=225)
+    gender = models.BooleanField()
+    birth_day=models.DateField()

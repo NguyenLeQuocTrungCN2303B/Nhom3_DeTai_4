cddf95a7d453c03d6d465a20fc27c043f26c5bdb
anh phu <phudz25022005@gmail.com>
2025-02-05 00:06:16 +0700

    show data

--- Project/my_app/home/views.py
+++ Project/my_app/home/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from A_Product_Mng.models import Product
 # Create your views here.
 def home(request):
-    return render(request , 'home/home.html')
+    return render(request , 'home.html')
+
+def products(request):
+    products = Product.objects.all()
+    template = loader.get_template('home/products.html')
+    context ={
+        'products':products,
+    }
+    return HttpResponse(template.render(context,request))
+

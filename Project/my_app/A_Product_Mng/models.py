9b3f47a61455973815f84ed71015aa6f785a66b2
anh phu <phudz25022005@gmail.com>
2025-02-11 13:40:50 +0700

    create car-page

--- Project/my_app/A_Product_Mng/models.py
+++ Project/my_app/A_Product_Mng/models.py
@@ -7,6 +7,7 @@ class Product (models.Model):
     weight = models.FloatField()
     wage_price = models.FloatField()
     Stone_price= models.FloatField()
+    price = models.FloatField(null=True, blank=True)
     digital = models.BooleanField(default=False,null=True, blank=False)
     image= models.ImageField(null=True, blank=True)
 
@@ -19,6 +20,9 @@ class Product (models.Model):
         except:
             url = ''
         return url
+    @property
+    def total_price(self,gold_price):
+        return (self.wage_price + self.Stone_price + self.weight*gold_price)
 
 class Customer (models.Model):
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=False)
@@ -36,12 +40,26 @@ class Order (models.Model):
 
     def __str__(self):
         return str(self.id)
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
     
 class OrderItem (models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress (models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
